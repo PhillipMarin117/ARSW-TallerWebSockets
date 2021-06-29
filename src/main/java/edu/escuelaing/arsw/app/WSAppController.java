080a7cdfbae888a17b23969053b0c1ef652b33f5
Nickolas Marin <nickolas.marin@mail.escuelaing.edu.co>
2021-06-29 12:02:58 -0500

    Taller terminado funcionalmente

---
+++ src/main/java/edu/escuelaing/arsw/app/WSAppController.java
@@ -0,0 +1,12 @@
+package edu.escuelaing.arsw.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WSAppController {
+
+    public static void main(String[] args){
+        SpringApplication.run(WSAppController.class, args);
+    }
+}
